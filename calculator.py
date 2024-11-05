7e7b1b0de96ca219dcf5526d4c8b2859ba1e90a4
Scheraz <sherazkhanpk.sk@gmail.com>
2024-11-05 23:53:13 +0500

    final push

---
+++ calculator.py
@@ -0,0 +1,58 @@
+logo = r"""
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+print(logo)
+
+
+
+def add(n1, n2):
+    return n1 + n2
+def subtract(n1,n2):
+    return n1 - n2
+def multiply(n1,n2):
+    return n1 * n2
+def divide(n1,n2):
+    return n1 / n2
+
+operation = {
+    "+" : add,
+    "-" : subtract,
+    "*" : multiply,
+    "/" : divide}
+
+def calculator():
+    should_accumulate = True
+    num1 = float(input("Enter First Number: "))
+    for symbol in operation:
+        print(symbol)
+    while should_accumulate:
+        operation_symbol = input("Pick any operation: ")
+        num2 = float(input("What's the second number?: "))
+        answer = operation[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        choice = input("Type 'y' to continue calculating with 8.0, or type 'n' to start a new calculation:")
+
+        if choice == "y":
+            num1 = answer
+        elif choice == "n":
+            should_accumulate = False
+            calculator()
+calculator()
+
+
+
+
+
